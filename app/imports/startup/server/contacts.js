5d710298973ca30a0f7262258f3fda71e462f1c9
Gabriel L. Viloria <gviloria@hawaii.edu>
2016-10-16 18:30:16 -1000

    Finished Contacts Collection and Home Page Update

---
+++ app/imports/startup/server/contacts.js
@@ -0,0 +1,42 @@
+/**
+ * Created by gviloria on 10/16/16.
+ */
+import { Contacts } from '../../api/stuff/contacts.js';
+import { _ } from 'meteor/underscore';
+
+/**
+ * A list of Stuff to pre-fill the Collection.
+ * @type {*[]}
+ */
+const contactSeeds = [
+  {
+    first: 'Gabriel',
+    last: 'Viloria',
+    address: '3161 Ala Ilima St',
+    telephone: '808-218-9262',
+    email: 'gviloria@hawaii.edu',
+  },
+  {
+    first: 'Angelo',
+    last: 'Viloria',
+    address: '808-868 Ala Ilima Place',
+    telephone: '808-976-7754',
+    email: 'angelov@hawaii.edu',
+  },
+  {
+    first: 'Sally',
+    last: 'Viloria',
+    address: '45-419 Makalani St',
+    telephone: '808-247-5385',
+    email: 'sviloria@hawaii.edu',
+  },
+];
+
+/**
+ * Initialize the Stuff collection if empty with seed data.
+ */
+if (Contacts.find().count() === 0) {
+  _.each(contactSeeds, function seedContacts(stuff) {
+    Contacts.insert(stuff);
+  });
+}
